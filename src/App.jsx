019a93ce8e077e6dd31c3893a21bdcad7126473b
alan <alan@iMac-de-alan.local>
2024-04-22 15:36:03 -0600

    first commit
    created vite project and add a email password form

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <form action="">
+      <label htmlFor="email">email:
+      <input type="email" name="email" id="email" />
+      </label>
+        <label htmlFor="password">Password
+        <input type="password" name="password" id="password" />
+        </label>
+     </form>
+    </>
+  )
+}
+
+export default App
